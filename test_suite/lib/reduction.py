44796fcacce5b768be50f96fa813db7e79cd29cb
Liza Sazonova <sazonova.astro@gmail.com>
2025-10-29 15:27:35 -0400

    bg bug fix

--- test_suite/lib/reduction.py
+++ test_suite/lib/reduction.py
@@ -8,12 +8,8 @@ from skimage import transform as T
 from astropy.io import fits
 from astropy.wcs import WCS
 from astropy import units as u
-from astropy.stats import sigma_clipped_stats
 
-from photutils.segmentation import detect_sources
-from photutils.background import Background2D
-
-from utils import grow_segmask
+from utils import estimate_bg
 
 
 ##### Loading and preparing raw data ##########
@@ -111,94 +107,7 @@ def load_raw_data(row, ignore_ctx=True, path='../data'):
 
     return img, mask, chipmask, exptime, psf, header
 
-def estimate_bg(img, mask, chipmask, max_iter=30, thresh=0.05, fit_bg=True, masked_thresh=0.2, filter_size=3):
-    """Estimates the large-scale background on a smoothed image iteratively 
-    to ensure that all of the object flux is properly masked.
-    First, background stats are estimated on a raw image. Then, we run source detection
-    to find all sources 1sigma above the background, and mask these. Then, we re-calculate
-    the background stats. Continue this until the background sigma converges.
-    Finally, use photutils Background2D to estimate the 2D background and subtract it.
-    
-    Args:
-        img: NxN np.array with the full tile
-        mask: NxN mask showing bad pixels etc
-        chipmask: similar to mask, where 1 shows the detector area and 0 blank fields
-        max_iter: the number of times we recalculate mask and background before convergence (Default: 10)
-        thresh: the convergence threshold in % difference between background error
-        fit_background (bool): if True, also run the 2D background fit (original images only)
-        masked_thresh (float): minimum number of pixels that should be unmasked for BG estimation
-        filter_size (int): size of the filter to used in smoothing the image for source detection
-    Returns:
-        img: background-subtracted image
-        median: background median
-        std: background standard deviation
-        bg_mask: mask (including the detected sources) used to calculate BG
-        bg: NxN background image
-    """
 
-    img_conv = ndi.gaussian_filter(img, sigma=filter_size, truncate=2)
-
-    # First pass at getting the background RMS
-    mean, median, std = sigma_clipped_stats(img, sigma=3.0, mask=mask)
-    std_og = std 
-
-    detect_area = 10
-    # Recursively mask sources and re-calculate bg while we do not converge
-    for i in range(max_iter):
-        std_prev = std
-    
-        segmap = detect_sources(img_conv, 1*std+median, npixels=detect_area, mask=chipmask, connectivity=4)
-        segmask = segmap.data > 0
-
-        # Stop if there are fewer than 20% unmasked pixels
-        masked_frac = np.sum(segmask | mask)/img.size
-        if masked_frac > (1-masked_thresh):
-            break
-        
-        mean, median, std = sigma_clipped_stats(img, sigma=3.0, mask=mask|segmask)
-
-        err = np.abs(std-std_prev)/std_prev
-        if err < thresh: break
-
-    
-    # Grow the mask edges by an amount relative to the segment size
-    segmask = grow_segmask(segmap, grow_sigma=3, area_norm=1)
-    bg_mask = mask|segmask
-
-    if fit_bg:
-        # Estimate 2D background
-        box_size = int(img.shape[0]*0.01)
-        bkg = Background2D(img, (box_size,box_size), filter_size=3, mask=bg_mask, coverage_mask=chipmask)
-
-        fig, axs = plt.subplots(1,3,figsize=(15,5))
-        axs[0].imshow(img, vmin=median-std, vmax=median+std, cmap='gray')
-        axs[1].imshow(bkg.background, vmin=median-std, vmax=median+std, cmap='gray')
-        axs[2].imshow(img-bkg.background, vmin=-std, vmax=std, cmap='gray')
-        for ax in axs:
-            ax.contour((segmask | chipmask)>0, colors=['C9'], levels=[0.9,1.1], linewidths=0.5)
-        plt.show()
-
-        answer = input("Subtract background? y | n\n")
-        if answer == 'y': 
-            img -= bkg.background
-            std = bkg.background_rms_median
-            bg_std_map = bkg.background_rms
-        else:
-            mean, median, std = sigma_clipped_stats(img, sigma=3.0, mask=bg_mask)
-            img -= median
-            bg_std_map = std*np.ones_like(img)
-    else:
-        mean, median, std = sigma_clipped_stats(img, sigma=3.0, mask=bg_mask)
-        img -= median
-        bg_std_map = std*np.ones_like(img)
-    
-    # Just return the normal non-iterative background if didn't reach convergence
-    if i==max_iter-1: 
-        print("WARNING! Background didn't converge. Returning ismple sigma-clip background")
-        std = std_og
-    
-    plt.close()
-    return img, median, std, bg_mask, bg_std_map
 
 def make_cutout_slice(row, img_shape, header, frac_r=3):
     """ Create a slice object to make cutouts later on based on the galaxy ra, dec, and Rpet. The cutout is 
@@ -242,15 +151,17 @@ def save_cutout(row, img, exp, mask, psf, bg_std, bg_std_map, header, path='../d
     err_cutout = err[cutout_slice]
     mask_cutout = mask[cutout_slice]
     bg_cutout = bg_std_map[cutout_slice]
+    exp_cutout = exp[cutout_slice]
 
     # Create a new FITS file and save the data
     hdu_img = fits.PrimaryHDU(img_cutout, header=header)
     hdu_err = fits.ImageHDU(err_cutout, name='ERR')
     hdu_mask = fits.ImageHDU(mask_cutout.astype(np.uint8), name='MASK')
     hdu_bg = fits.ImageHDU(bg_cutout, name='BG_RMS')
+    hdu_exp = fits.ImageHDU(exp_cutout, name='EXP')
     hdu_psf = fits.ImageHDU(psf, name='PSF')
 
-    hdul = fits.HDUList([hdu_img, hdu_err, hdu_mask, hdu_psf, hdu_bg])
+    hdul = fits.HDUList([hdu_img, hdu_err, hdu_mask, hdu_psf, hdu_bg, hdu_exp])
     hdul.writeto(f'{path}/cutouts/{row.galaxy}.fits', overwrite=True)
 
     # Make a diagnostic figure of the cutouts
@@ -289,7 +200,6 @@ if __name__ == '__main__':
     parser.add_argument('--ignore_ctx', help='Mask non-zero CTX pixels?', action="store_true")
     args = parser.parse_args()
 
-
     catalog = pd.read_csv('../data/catalogs/sample.csv')
     row = catalog[catalog.galaxy==args.galaxy].iloc[0]
     reduce_galaxy(row, ignore_ctx=args.ignore_ctx, path='../data')
@@ -297,11 +207,3 @@ if __name__ == '__main__':
 
 
 
-
-##### Augmentations ##########
-# 1. Load the image cutout
-# 2. Rescale it to the desired physical resolution
-# 3. Re-estimate the background / surface brightness limit 
-# 4. Add noise to match the desired SB limit
-# 5. Save the augmented image
-###############################################
